2f8c4dd20275e73b3c491d78cd591c9edcae76ac
CDwee <96454637+CDwee@users.noreply.github.com>
2022-06-16 22:27:18 -0500

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,251 @@
+<!-- Started at 8:24 6-16-2022 -->
+
+<?php
+	include("includes/classes/Account.php");
+	include("includes/classes/Constants.php");
+
+	$account = new Account();
+
+	include("includes/handlers/register-handler.php");
+	include("includes/handlers/login-handler.php");
+
+	function getInputValue($name) {
+		if(isset($_POST[$name])) {
+			echo $_POST[$name];
+		}
+	}
+?>
+
+<html>
+<head>
+	<title>Welcome to Slotify!</title>
+</head>
+<body>
+
+	<div id="inputContainer">
+		<form id="loginForm" action="register.php" method="POST">
+			<h2>Login to your account</h2>
+			<p>
+				<label for="loginUsername">Username</label>
+				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password</label>
+				<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="loginButton">LOG IN</button>
+			
+		</form>
+
+
+
+		<form id="registerForm" action="register.php" method="POST">
+			<h2>Create your free account</h2>
+			<p>
+				<?php echo $account->getError(Constants::$usernameCharacters); ?>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" value="<?php getInputValue('username') ?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$firstNameCharacters); ?>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" value="<?php getInputValue('firstName') ?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$lastNameCharacters); ?>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" value="<?php getInputValue('lastName') ?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$emailsDoNotMatch); ?>
+				<?php echo $account->getError(Constants::$emailInvalid); ?>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email') ?>" required>
+			</p>
+
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" value="<?php getInputValue('email2') ?>" required>
+			</p>
+
+			<p>
+				<?php echo $account->getError(Constants::$passwordsDoNoMatch); ?>
+				<?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+				<?php echo $account->getError(Constants::$passwordCharacters); ?>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password" required>
+			</p>
+
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="registerButton">SIGN UP</button>
+			
+		</form>
+
+
+	</div>
+
+</body>
+</html>
+
+<?php 
+
+function sanitizeFormPassword($inputText) {
+	$inputText = strip_tags($inputText);
+	return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	return $inputText;
+}
+
+
+if(isset($_POST['registerButton'])) {
+	//Register button was pressed
+	$username = sanitizeFormUsername($_POST['username']);
+	$firstName = sanitizeFormString($_POST['firstName']);
+	$lastName = sanitizeFormString($_POST['lastName']);
+	$email = sanitizeFormString($_POST['email']);
+	$email2 = sanitizeFormString($_POST['email2']);
+	$password = sanitizeFormPassword($_POST['password']);
+	$password2 = sanitizeFormPassword($_POST['password2']);
+
+	$wasSuccessful = $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
+
+	if($wasSuccessful == true) {
+		header("Location: index.php");
+	}
+
+}
+
+
+?>
+
+<?php
+	class Account {
+
+		private $errorArray;
+
+		public function __construct() {
+			$this->errorArray = array();
+		}
+
+		public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
+			$this->validateUsername($un);
+			$this->validateFirstName($fn);
+			$this->validateLastName($ln);
+			$this->validateEmails($em, $em2);
+			$this->validatePasswords($pw, $pw2);
+
+			if(empty($this->errorArray) == true) {
+				//Insert into db
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		}
+
+		public function getError($error) {
+			if(!in_array($error, $this->errorArray)) {
+				$error = "";
+			}
+			return "<span class='errorMessage'>$error</span>";
+		}
+
+		private function validateUsername($un) {
+
+			if(strlen($un) > 25 || strlen($un) < 5) {
+				array_push($this->errorArray, Constants::$usernameCharacters);
+				return;
+			}
+
+			//TODO: check if username exists
+
+		}
+
+		private function validateFirstName($fn) {
+			if(strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorArray, Constants::$firstNameCharacters);
+				return;
+			}
+		}
+
+		private function validateLastName($ln) {
+			if(strlen($ln) > 25 || strlen($ln) < 2) {
+				array_push($this->errorArray, Constants::$lastNameCharacters);
+				return;
+			}
+		}
+
+		private function validateEmails($em, $em2) {
+			if($em != $em2) {
+				array_push($this->errorArray, Constants::$emailsDoNotMatch);
+				return;
+			}
+
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorArray, Constants::$emailInvalid);
+				return;
+			}
+
+			//TODO: Check that username hasn't already been used
+
+		}
+
+		private function validatePasswords($pw, $pw2) {
+			
+			if($pw != $pw2) {
+				array_push($this->errorArray, Constants::$passwordsDoNoMatch);
+				return;
+			}
+
+			if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+				array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
+				return;
+			}
+
+			if(strlen($pw) > 30 || strlen($pw) < 5) {
+				array_push($this->errorArray, Constants::$passwordCharacters);
+				return;
+			}
+
+		}
+
+
+	}
+?>
+
+<?php
+class Constants {
+
+	public static $passwordsDoNoMatch = "Your passwords don't match";
+	public static $passwordNotAlphanumeric = "Your password can only contain numbers and letters";
+	public static $passwordCharacters = "Your password must be between 5 and 30 characters";
+	public static $emailInvalid = "Email is invalid";
+	public static $emailsDoNotMatch = "Your emails don't match";
+	public static $lastNameCharacters = "Your last name must be between 2 and 25 characters";
+	public static $firstNameCharacters = "Your first name must be between 2 and 25 characters";
+	public static $usernameCharacters = "Your username must be between 5 and 25 characters";
+
+}
+?>
+
+<!-- Ended at 10:27 6-16-2022 -->
